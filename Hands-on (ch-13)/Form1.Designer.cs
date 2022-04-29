8c51048ea346a603f1fee19480ece2457924f7f1
alvaldes86 <76670909+alvaldes86@users.noreply.github.com>
2022-04-28 22:11:45 -0400

    Add project files.

---
+++ Hands-on (ch-13)/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Hands_on__ch_13_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboOperation = new System.Windows.Forms.ComboBox();
+            this.txtValue1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtValue2 = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(58, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Value 1:";
+            // 
+            // cboOperation
+            // 
+            this.cboOperation.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboOperation.FormattingEnabled = true;
+            this.cboOperation.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.cboOperation.Location = new System.Drawing.Point(219, 121);
+            this.cboOperation.Name = "cboOperation";
+            this.cboOperation.Size = new System.Drawing.Size(167, 28);
+            this.cboOperation.TabIndex = 1;
+            // 
+            // txtValue1
+            // 
+            this.txtValue1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValue1.Location = new System.Drawing.Point(219, 49);
+            this.txtValue1.Name = "txtValue1";
+            this.txtValue1.Size = new System.Drawing.Size(167, 27);
+            this.txtValue1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(58, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Operation:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(58, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Value 2:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(58, 266);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Result:";
+            // 
+            // lblResult
+            // 
+            this.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(215, 266);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(171, 33);
+            this.lblResult.TabIndex = 6;
+            // 
+            // txtValue2
+            // 
+            this.txtValue2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValue2.Location = new System.Drawing.Point(219, 195);
+            this.txtValue2.Name = "txtValue2";
+            this.txtValue2.Size = new System.Drawing.Size(167, 27);
+            this.txtValue2.TabIndex = 7;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(62, 369);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(117, 39);
+            this.btnCalculate.TabIndex = 8;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(249, 369);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(117, 39);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(434, 369);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(117, 39);
+            this.btnExit.TabIndex = 10;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(613, 461);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtValue2);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtValue1);
+            this.Controls.Add(this.cboOperation);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboOperation;
+        private System.Windows.Forms.TextBox txtValue1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txtValue2;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Hands-on (ch-13)/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hands_on__ch_13_
+{
+    delegate double myDelegate(double value1, double value2);
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtValue1.Clear();
+            txtValue2.Clear();
+            cboOperation.SelectedIndex = -1;
+            lblResult.ResetText();
+
+            txtValue1.Focus();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //performOperation();
+            //performOperationWithLambda();
+            performOperationWithBuiltIns();
+        }
+        
+        
+        private void performOperation()
+        {
+            if(!double.TryParse(txtValue1.Text, out double value1))
+            {
+                txtValue1.SelectAll();
+                txtValue1.Focus();
+                lblResult.Text = "Enter a value";
+            }
+            else if(cboOperation.SelectedIndex == -1)
+            {
+                lblResult.Text = "Select an operation";
+            }
+            else if(!double.TryParse(txtValue2.Text, out double value2))
+            {
+                txtValue2.SelectAll();
+                txtValue2.Focus();
+                lblResult.Text = "Enter a value";
+            }
+            else
+            {
+                myDelegate add = delegate (double number1, double number2)
+                { return number1 + number2; };
+
+                myDelegate subtract = delegate (double number1, double number2)
+                { return number1 - number2; };
+
+                myDelegate multiply = delegate (double number1, double number2)
+                { return number1 * number2; };
+
+                myDelegate divide = delegate (double number1, double number2)
+                { return number1 / number2; };
+
+                myDelegate myOperation;
+
+                if(cboOperation.SelectedItem.ToString() == "+")
+                    myOperation = add;
+                else if(cboOperation.SelectedItem.ToString() == "-")
+                    myOperation = subtract;
+                else if(cboOperation.SelectedItem.ToString() == "*")
+                    myOperation = multiply;
+                else
+                    myOperation = divide;
+
+                double result = myOperation(value1, value2);
+
+                lblResult.Text = result.ToString();
+            }
+        }
+
+        private void performOperationWithLambda()
+        {
+            if (!double.TryParse(txtValue1.Text, out double value1))
+            {
+                txtValue1.SelectAll();
+                txtValue1.Focus();
+                lblResult.Text = "Enter a value";
+            }
+            else if (cboOperation.SelectedIndex == -1)
+            {
+                lblResult.Text = "Select an operation";
+            }
+            else if (!double.TryParse(txtValue2.Text, out double value2))
+            {
+                txtValue2.SelectAll();
+                txtValue2.Focus();
+                lblResult.Text = "Enter a value";
+            }
+            else
+            {
+                myDelegate add = (number1, number2) => number1 + number2;
+                myDelegate subtract = (number1, number2) => number1 - number2;
+                myDelegate multiply = (number1, number2) => number1 * number2;
+                myDelegate divide = (number1, number2) => number1 / number2;
+                myDelegate unknown = (number1, number2) => 0;
+
+                myDelegate myOperation;
+
+                switch (cboOperation.SelectedItem.ToString())
+                {
+                    case "+":
+                        myOperation = add;
+                        break;
+                    case "-":
+                        myOperation = subtract;
+                        break;
+                    case "*":
+                        myOperation = multiply;
+                        break;
+                    case "/":
+                        myOperation = divide;
+                        break;
+                    default:
+                        myOperation = unknown;
+                        break;
+                }
+
+                double result = myOperation(value1, value2);
+                lblResult.Text = result.ToString();
+            }
+        }
+        //using .NET built delegate Func. Also look at the other two built-in .net delegates Action and Predicate
+        private void performOperationWithBuiltIns()
+        {
+            Action<string> displayResult = (str) => lblResult.Text = str;
+            Predicate<int> notSelected = (i) => i == -1;
+
+            if (!double.TryParse(txtValue1.Text, out double value1))
+            {
+                txtValue1.SelectAll();
+                txtValue1.Focus();
+                lblResult.Text = "Enter a value";
+            }
+            else if (notSelected(cboOperation.SelectedIndex))//using the .Net built-in Predicate delegate
+            {
+                lblResult.Text = "Select an operation";
+            }
+            else if (!double.TryParse(txtValue2.Text, out double value2))
+            {
+                txtValue2.SelectAll();
+                txtValue2.Focus();
+                lblResult.Text = "Enter a value";
+            }
+            else
+            {
+                //the last data type represents the return type
+                Func<double, double, double> add = (number1, number2) => number1 + number2;
+                Func<double, double, double> subtract = (number1, number2) => number1 - number2;
+                Func<double, double, double> multiply = (number1, number2) => number1 * number2;
+                Func<double, double, double> divide = (number1, number2) => number1 / number2;
+                Func<double, double, double> unknown = (number1, number2) => 0;
+
+
+                switch (cboOperation.SelectedItem.ToString())
+                {
+                    case "+":
+                        lblResult.Text = add(value1,value2).ToString();
+                        break;
+                    case "-":
+                        lblResult.Text = subtract(value1, value2).ToString();
+                        break;
+                    case "*":
+                        lblResult.Text = multiply(value1, value2).ToString();
+                        break;
+                    case "/":
+                        lblResult.Text = divide(value1, value2).ToString();
+                        break;
+                    default:
+                        lblResult.Text = unknown(value1, value2).ToString();
+                        break;
+                }
+
+            }
+
+        }
+    }
+}
